68c3477b90820fdd95724e50f6782b279047d504
lucasmanea21 <lucasmanea@Yahoo.com>
2021-06-24 11:25:44 +0300

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
+
 dotenv.config()
 const app = express()
 
@@ -14,10 +16,7 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes)
-
-app.get('/', (req,res) => {
-    res.send('Hello to memories API')
-})
+app.use('/user', userRoutes)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
